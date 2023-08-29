da5eb2be73fad0cbd9fc18c9a7704924e6919b1f
Mikhail <m.popkov.02@mail.ru>
2023-08-29 17:08:50 +0300

    Добавление таймера

--- js/script.js
+++ js/script.js
@@ -33,4 +33,51 @@ window.addEventListener("DOMContentLoaded", function () {
       }
     }
   });
+
+  // Timer
+
+  let deadLine = "2023-09-01";
+
+  function getTime(endtime) {
+    let time = Date.parse(endtime) - Date.parse(new Date()),
+      seconds = Math.floor((time / 1000) % 60),
+      minutes = Math.floor((time / 1000 / 60) % 60),
+      hours = Math.floor((time / 1000 / 60 / 60) % 24),
+      days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+    if (seconds < 10) seconds = "0" + seconds;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (hours < 10) hours = "0" + hours;
+
+    return {
+      total: time,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+      days: days,
+    };
+  }
+
+  function startTimer(id, endtime) {
+    let timer = document.getElementById(id),
+      days = timer.querySelector(".days"),
+      hours = timer.querySelector(".hours"),
+      minutes = timer.querySelector(".minutes"),
+      seconds = timer.querySelector(".seconds"),
+      getInterval = setInterval(getNewTime, 1000);
+
+    function getNewTime() {
+      let t = getTime(endtime);
+      days.textContent = t.days;
+      hours.textContent = t.hours;
+      minutes.textContent = t.minutes;
+      seconds.textContent = t.seconds;
+
+      if (t.total <= 0) {
+        clearInterval(getInterval);
+      }
+    }
+  }
+
+  startTimer("timer", deadLine);
 });
